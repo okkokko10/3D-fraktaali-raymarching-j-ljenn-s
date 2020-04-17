3de25baa0ab7f4edffcce4bebdac50b18061e6a9
okkokko10 <okko.heinio@gmail.com>
2020-04-18 01:31:16 +0300

    lisätty kunnollinen liike, ja osittain peilejä
    peilaus kesken,
    hetkellisesti R sijoittaa palikan kameran alapuolelle.

--- RaymarchingAnd3DFractals/Form1.cs
+++ RaymarchingAnd3DFractals/Form1.cs
@@ -20,26 +20,30 @@ namespace RaymarchingAnd3DFractals
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            Vector3 moveControl = new Vector3(0, 0, 0); //Vector3.zero;
             if (e.KeyCode == Keys.W)
             {
-                pointOfView += new Vector3(direction.x, direction.y, direction.z);
-                //pointOfView.z += 1;
-                //RaisePaintEvent(Form1_Paint,);
+                moveControl.z += 1;
             }
             if (e.KeyCode == Keys.S)
             {
-                pointOfView -= new Vector3(direction.x, direction.y, direction.z);
-                //pointOfView.z -= 1;
+                moveControl.z += -1;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveControl.x += 1;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                moveControl.x += -1;
             }
             if (e.KeyCode == Keys.Down)
             {
-                //pointOfView.x -= 1;
-                rot_up -= 0.1f;
+                rot_up -= 0.2f;
             }
             if (e.KeyCode == Keys.Up)
             {
-                //pointOfView.x += 1;
-                rot_up += 0.1f;
+                rot_up += 0.2f;
             }
             if (e.KeyCode == Keys.Space)
             {
@@ -51,36 +55,31 @@ namespace RaymarchingAnd3DFractals
             }
             if (e.KeyCode == Keys.Right)
             {
-                //direction.x += 1;
-                rot_side += 0.1f;
+                rot_side -= 0.2f;
             }
             if (e.KeyCode == Keys.Left)
             {
-                //direction.x -= 1;
-                rot_side -= 0.1f;
+                rot_side += 0.2f;
             }
             if (e.KeyCode == Keys.R)
             {
-                drawnObject.Position.y -= 0.1f;
+                drawnObject.Position = pointOfView*1;
+                drawnObject.Position.y -= 01f;
             }
+
+            pointOfView += moveControl.Rotate(rot_side, rot_up);
 
             FindForm().Refresh();
             
         }
+        public const int resolution_X = 150;
+        public const int resolution_Y = 150;
+
 
         public float rot_up = 0;
         public float rot_side = 0;
-        //public Vector3 direction = new Vector3(-0f, -0f, 1f);
-        public Vector3 direction
-        {
-            get
-            {
-                return new Vector3(0, 0, 1).Rotate(rot_side, rot_up);
-                return Vector3.TowardsAngle(rot_side, rot_up);
-            }
-        }
-        public Vector3 pointOfView = new Vector3(0-5f, 0f, -0f);
-        public Rayable drawnObject = new Rayable(new Vector3(0f, 01f, 0f),new Vector3(1,1,01)*0.5f, 0.0f); //why the f does the results change drastically if I put in a fraction instead of a decimal?
+        public Vector3 pointOfView = new Vector3(05f, 0f, 0f);
+        public Rayable drawnObject = new Rayable(new Vector3(2f, 01f, 1f),new Vector3(2,1,1)*0.5f, 0.0f); //why the f does the results change drastically if I put in a fraction instead of a decimal?
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
@@ -88,7 +87,7 @@ namespace RaymarchingAnd3DFractals
             Graphics g = e.Graphics;
             Bitmap image1 = new Bitmap(Image.FromFile(@"C:\Users\Okko Heiniö\Documents\My Games\Terraria\ModLoader\Mod Sources\OkkokkoHeap\Textures\500x500.bmp"));
 
-            Bitmap image2 = Scan(image1, drawnObject, pointOfView, direction);
+            Bitmap image2 = Scan(image1, drawnObject, pointOfView);
             g.DrawImage(image2, new Rectangle(0,0,Width*500/resolution_X,Height*500/resolution_Y));
                     //g.Flush(System.Drawing.Drawing2D.FlushIntention.Flush);
             e.Dispose();
@@ -99,19 +98,28 @@ namespace RaymarchingAnd3DFractals
             Vector3 point = new Vector3(Point.x,Point.y,Point.z);
             if (true)
             {
-                //if (point.x < 3f && point.x > -3f)
+                //if (point.x < 4f && point.x > -5f)
                 {
-                    //point.x = MathModulo(point.x, 2f);
+                    //point.x = MathModulo(point.x+1f, 2f)-1f;
                 }
                 //if (point.y < 3f && point.y > -3f)
                 {
                     //point.y = MathModulo(point.y, 2f);
                     //point.y = Math.Abs(point.y);
                 }
-                //if (point.y < 0)
+                if (point.y < 0)
                 {
                     //point.y = -point.y;
                 }
+                if (point.x < 0)
+                {
+                    point.x = -point.x;
+                }
+
+                //point.x = 2 - Math.Abs(Math.Abs(point.x - 2) - 4);
+                point = Mirror(point - new Vector3(0, 0, 2), 1) + new Vector3(0, 0, 2);
+                point = Mirror(point - new Vector3(0, 0, -2), -1) + new Vector3(0, 0, -2);
+
                 //if (point.y > 2){ point.y -= 2f;point.x -= 1f;}
                 //point.y = Math.Abs(point.y);
                 //point.z = Math.Abs(10 - point.z);
@@ -123,17 +131,22 @@ namespace RaymarchingAnd3DFractals
         {
             return ((a % b) + b) % b;
         }
-
-        public Bitmap Scan(Bitmap baseImage, Rayable drawnObject, Vector3 pov = null, Vector3 rotation = null,float lengthCutoff=30)
+        public Vector3 Mirror(Vector3 point,int a)//,Vector3 direction)
         {
-            if (pov == null) { pov = Vector3.zero; }
-            if (rotation == null) { rotation = new Vector3(0, 0, 1); }
+            //int a = 1;
+            if (point.x < a * point.z) return new Vector3(a * point.z, point.y, a * point.x);
+            else return point;
+        }
+
+
+        public Bitmap Scan(Bitmap baseImage, Rayable drawnObject, Vector3 pov,float lengthCutoff=30)
+        {
             Bitmap image = new Bitmap( baseImage);
             for (int iy = 1; iy < resolution_Y; iy++)
             {
                 for (int ix = 1; ix < resolution_X; ix++)
                 {
-                    RayResult result = Ray(RayDirection(ix,iy,rotation), drawnObject, pov, 0.005f, lengthCutoff,30);
+                    RayResult result = Ray(RayDirection(ix,iy), drawnObject, pov, 0.005f, lengthCutoff,50);
                     //int color = (int)((1 - (result.Closest /0.05f)) * 255);
                     //int color = (int)((1 - (result.RayLength / 20)) * 255);
                     //int color = (int)(( (result.RayLength/lengthCutoff)) * 255);
@@ -142,16 +155,14 @@ namespace RaymarchingAnd3DFractals
                     if (color < 0) { color = 0; }
                     {
                         Color color1 = Color.FromArgb(color, color, color);
-                        image.SetPixel(resolution_X-ix, resolution_Y-iy, color1);
+                        //image.SetPixel(resolution_X-ix, resolution_Y-iy, color1);
+                        image.SetPixel(ix, resolution_Y - iy, color1);
                         //DrawPixel(baseImage, color1, ix, resolution_Y - iy);
                     }
                 }
             }
             return image;
         }
-        public const int resolution_X = 150;
-        public const int resolution_Y = 150;
-
         public void DrawPixel(Bitmap bitmap, Color color, int px, int py)
         {
 
@@ -161,7 +172,7 @@ namespace RaymarchingAnd3DFractals
 
         }
 
-        public Vector3 RayDirection(int px,int py,Vector3 rotation)
+        public Vector3 RayDirection(int px,int py)
         {
             return new Vector3((float)(px) / resolution_X - 0.5f, (float)(py) / resolution_Y - 0.5f, 01f).Rotate(rot_side,rot_up).Normalize;
 
@@ -239,7 +250,7 @@ namespace RaymarchingAnd3DFractals
             {
                 get
                 {
-                    return new Vector3(this.x,this.y,this.z);
+                    return new Vector3(this.x, this.y, this.z);
                 }
             }
             public Vector3 Normalize
@@ -282,7 +293,7 @@ namespace RaymarchingAnd3DFractals
                 return rotation + projX + projY; //-x,y,1
 
             }
-            public Vector3 Rotate(float a,float b)
+            public Vector3 Rotate(float a, float b)
             {
                 //  (c,d,r) side=a,up=b
                 //  (cos(a)cos(b)c-sin(a)r-cos(a)sin(b)d,sin(b)c+cos(b)d,sin(a)cos(b)c+cos(a)r-sin(a)sin(b)d)
@@ -310,6 +321,7 @@ namespace RaymarchingAnd3DFractals
 
             }
             public static Vector3 zero = new Vector3(0, 0, 0);
+            
             public Vector3 Positive
             {
                 get
